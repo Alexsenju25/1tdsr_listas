876a2b9e762e1a422e833ca3a9453334b579dd11
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-15 20:24:25 -0300

    Exercicios concluidos.

--- 20240315_1TDSR/exercicio4.py
+++ 20240315_1TDSR/exercicio4.py
@@ -8,16 +8,17 @@
 #Calcula e mostra o valor recebido a título de hora-extra (se houver) e o salário nal do
 #trabalhador.
 
-dias_uteis = int(input("Qual o total de dias úteis do mes? "))
-total_horas_trabalhadas = int(input("Qual o total de horas trabalhadas? "))
-recebe_hora = int(input("Quanto o trabalhador recebe por hora? "))
+dias_uteis = float(input("Qual o total de dias úteis do mes? "))
+total_horas_trabalhadas = float(input("Qual o total de horas trabalhadas? "))
+recebe_hora = float(input("Quanto o trabalhador recebe por hora? "))
 
 horas_mensais = dias_uteis * 8
 horas_extras = total_horas_trabalhadas - horas_mensais
+valor_horas_extras =  (horas_extras * recebe_hora) * 0.5 + (horas_extras * recebe_hora)
 salario = recebe_hora * horas_mensais
-valor_horas_extras =  horas_extras * 0,5
+salario_mais_horas_extras = salario + valor_horas_extras
 
-if horas_extras == 0 :
-    print("Voce tem um salário de:",salario)
+if horas_extras <= 0 :
+    print("Voce tem um salário de:",salario_mais_horas_extras)
 else :
-    print("O seu salário é:", salario, "\nCom um total de:", valor_horas_extras, "horas extras.")
+    print("O seu salário é: R$", salario, "\nCom um total de:",horas_extras, "hora(s) extras, com o valor de: R$", valor_horas_extras, "\nCom um total de: R$", salario_mais_horas_extras)
