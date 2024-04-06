4e00eb584fffd42495b416050fdff7fcb61e2836
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 21:00:12 -0300

    att 04052024

---
+++ listas3/exercicio4_5.py
@@ -0,0 +1,10 @@
+n = int(input("Informe n: "))
+
+acumulador = 1
+i = 1
+while i <= n:
+    acumulador = acumulador * i
+    i = i + 1
+
+
+print(f"{n}!={acumulador}")
---
+++ listas3/treinamento_checkpoint/sistema_biblioteca.py
@@ -0,0 +1,46 @@
+import os
+
+os.system('cls')
+
+opcao = 0
+
+while opcao != 6:
+    print("Bem-vindo à Biblioteca FIAP")
+
+    print("1 - Reserva Livro\n2 - Cadastra Livro")
+    print("3 - Devolução\n4 - Consulta Livro")
+    print("1 - Cadastro do Leitor\n6 - Sair")
+    opcao = int(input("Digite a opção desejada: "))
+
+    if opcao == 1:
+        isdn = int(input("Informe o isdn do livro: "))
+        leitor = input("CPF do leitor: ")
+        print("Disponivel a partir do dia: 25/04/2024")
+        confirmacao = input("Deseja reservar (s/n)")
+        if confirmacao == "s":
+            print("Livro reservado, obrigado!")
+        else:
+            print("Livro não reservado obrigado")
+        while confirmacao != "s" or "n":
+            print("Opção errada, tente novamente.")
+    elif opcao == 2:
+        nome_livro = int(input("Digite o nome do livro: "))
+    elif opcao == 3:
+        pass
+    elif opcao == 4:
+        pass
+    elif opcao == 5:
+        nome = input("Digite seu nome: ")
+        cpf = input("Informe o CPF:")
+        celular = input("Número de telefone: ")
+        rm = input("RM: ")
+
+
+
+    elif opcao == 6:
+        print("Até logo!")
+    else:
+        print("Opção Inválida!\n")
+
+
+ 
